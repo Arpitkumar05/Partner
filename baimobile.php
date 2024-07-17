6fac70d0e2189850091ae23a77c9ab8e55c35763
Arpit Patel <arpitraj9753@gmail.com>
2024-07-17 18:02:21 +0530

    Initial commit

---
+++ baimobile.php
@@ -0,0 +1,118 @@
+<?php include 'header.php'; ?>
+
+<link rel="stylesheet" href="css/index.css">
+
+<section class="section-page-header">
+        <div class="header2">
+            <img src="images/baiMobileFrameworkProduct.jpg" alt="Rimus-Technology Logo" class="logo">
+            
+            
+</section>
+        
+<div class="compatibility">
+                <h2>Compatibility</h2>
+                <ul>
+                    <li>Citrix Workspace App</li>
+                    <li>Citrix Virtual Apps</li>
+                    <li>Citrix Virtual Device</li>
+                </ul>
+            </div>
+        </div>
+            
+            <div class="about">
+            <p>Our product  <b>baiMobile smartcard reader framework </b> adds smartcard
+                 based security into mobile applications. baiMobile Smartcard Reader
+                  Framework is a Software Development Kit (SDK) to strengthen Android
+                   and iOS applications with advanced security features by integrating 
+                   smartcard based operations. This enables mobile apps for secure 
+                   access of organization resources like network, applications,
+                    websites etc. With this framework, it becomes very easy for our 
+                    customers to integrate PKCS#11 (certificate, public and private key) 
+                    based operations like encryption, decryption of data, document and code
+                     signing etc. Our product is FIPS-2 compliant and supports industry 
+                     standards like PC/SC, ISO-7816-4, PIV, CAC, PKCS#11 & OpenSSL etc.</p>
+                     <br>
+                     <h3> <button onclick="window.open('pdf.pdf', '_blank')"> Download baiMobile product brochure</button></h3>
+                    </div>
+
+    <div class="navbar2">
+        <a href="#product-details" class="nav-link">Product Details</a>
+        <a href="#features" class="nav-link">Features</a>
+        <a href="#use-cases" class="nav-link">Use Cases</a>
+        <a href="#Technical-specifications" class="nav-link">Technical Specifications</a>
+    </div>
+    
+    <section id="product-details" class="section1">
+        <h2>Product Details</h2>
+        <p>baiMobile Reader Framework is a software library for iOS & Android 
+            to access smart card contents from within mobile applications. 
+            Mobile app developers can integrate baiMobile Smartcard Reader
+             Framework into the mobile applications to enhance the security
+              of their apps and ensure smartcard based logon, secure access 
+              of resources, applications, websites, emails & signing of documents. <br><br> 
+
+            Our product implements PKCS#11 layer to ease smart card operations.
+             Smartcard based capabilities can be used for smartcard based logon,
+              SSL client authentication, encryption/decryption, signing etc.
+            <br> 
+            Our baiMobile Smartcard Reader Framework (SDK), used by Fortune 100 companies, 
+            enables smartcard-based log-on into the mobile apps, offers cryptographic and 
+            certificate operations and is quickly becoming an industry standard for Android & iOS smartcard-based solutions.
+            The SDK is intended for mobile apps using the PKI environment. <br> <br>
+            Smartcard based mobile applications provide better integrity, safety & authenticity with combination of 'What you have' 
+            and 'What you know' which makes a perfect combination for data security.<br> baiMobile framework is an easy way to add smartcard-based 
+            security operations to your mobile apps.
+
+        </p>
+    </section>
+    
+    <section id="features" class="section">
+        <h2>Features</h2>
+        <ul>
+            <li> Secure access of company resources like network, applications, websites etc.</li>
+            <li> PKCS#11 module for card-specific plug-ins</li>
+            <li> Concurrent access of multiple readers</li>
+            <li> Abstracts the connection channel to the smartcard</li>
+            <li> Easy to integrate</li>
+            <li> Several of fortune 500 companies using this product</li>
+            <li> Bundled with useful samples</li>
+            <li> Available on Android & iOS</li>
+            <li> Support for both Bluetooth and cabled readers’ support</li>
+        </ul>
+    </section>
+    <section id="use-cases" class="section">
+        <h2>Use Cases</h2>
+        <ul>
+            <li> Smartcard based cryptographic operations</li>
+            <li> Adding stronger two factor authentication</li>
+            <li> Smartcard based login to user app</li>
+            <li> Smartcard based access control</li>
+            <li> Smartcard based encryption & signing</li>
+            <li> Smartcard based digital signing of emails & documents</li>
+            <li> VPN Authentication over mobile</li>
+            <li> Secure access of website using client authentication</li>
+            <li> Single sign-on, Multi-factor and ease in authentication</li>
+            <li> Offer service to integrate CCID reader into customer environment</li>
+        </ul>
+    </section>
+    <section id="Technical-specifications" class="section">
+        <h2>Technical Specifications</h2>
+        <ul>
+            <li> Java, C/C++ and Objective-C/Swift interfaces available for integration</li>
+            <li> Support for industry standard API’s (PC/SC, PKCS#11, OpenSSL)</li>
+            <li> Connection agnostic framework</li>
+            <li> Multiple concurrent interfaces supported</li>
+            <li> Reader technologies supported :<br>
+             <ol>    Bluetooth readers</ol> 
+            <ol>    USB readers</ol> </li>
+        <li> Support for multiple hardware tokens :<br>
+        <ol>    Smart card (contact and NFC)</ol> 
+        <ol>    SIM, SecureSD</ol>
+        </li>
+    </ul>
+    </section>
+    <script src="js/life.js"></script>
+
+    <?php include 'footer.php';?>
+
+    
